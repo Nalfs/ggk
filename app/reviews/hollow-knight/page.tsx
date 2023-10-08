e8047085a846346d071c0797f95a317b7da348fe
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-10-08 20:24:19 +0200

    pages

---
+++ app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HollowKnightPage() {
+  return (
+    <>
+      <h1>HollowKnightPage</h1>
+      <p>Reviews for Hollow Knight will be here</p>
+    </>
+  );
+}
