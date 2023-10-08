5cee4f92c98ab68b23a13961c638267b1b02406f
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-10-08 20:19:59 +0200

    page routes + css

---
+++ app/reviews/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ReviewsPage() {
+  return (
+    <>
+      <h1>Reviews</h1>
+      <p>Here we will list all the reviews</p>
+    </>
+  );
+}
---
+++ app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function StardewvalleyPage() {
+  return (
+    <>
+      <h1>StardewValleyPage</h1>
+      <p>Reviews for Stardew Valley will be here</p>
+    </>
+  );
+}
