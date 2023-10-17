732798a19a219a00e03d008974bb06e91d54afe0
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-10-17 19:41:41 +0200

    font optimization

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,9 +1,14 @@
 import type { ReactNode } from "react";
+import { orbitron } from "@/app/fonts";
 
 export interface HeadingProps {
   children: ReactNode;
 }
 
 export default function Heading({ children }: HeadingProps) {
-  return <h1 className="font-bold pb-3 text-2xl">{children}</h1>;
+  return (
+    <h1 className={`font-bold pb-3 text-2xl ${orbitron.className}`}>
+      {children}
+    </h1>
+  );
 }
