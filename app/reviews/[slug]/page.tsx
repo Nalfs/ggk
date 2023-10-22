b43318719d71a16b099f8be27aa16e4e58e41c5a
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-10-22 18:16:16 +0200

    added generateStaticParams

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import Heading from "@/components/Heading";
-import { getReview } from "@/lib/reviews";
+import { getReview, getSlugs } from "@/lib/reviews";
+interface ReviewPageParams {
+  slug: string;
+}
 
-export default async function ReviewPage(props: { params: { slug: string } }) {
-  const { title, date, image, body: html } = await getReview(props.params.slug);
+interface ReviewPageProps {
+  params: ReviewPageParams;
+}
+export async function generateStaticParams(): Promise<ReviewPageParams[]> {
+  const slugs = await getSlugs();
+  return slugs.map((slug) => ({ slug }));
+}
+
+export default async function ReviewPage({
+  params: { slug },
+}: ReviewPageProps) {
+  console.log("[ReviewPage]", slug);
+  const { title, date, image, body: html } = await getReview(slug);
   return (
     <>
       <Heading>{title}</Heading>
