639197b4058a335bb5f02121dc00c428363df7c8
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-10-29 22:34:05 +0100

    manual fetching and pagination

--- scripts/strapi_request.mjs
+++ scripts/strapi_request.mjs
@@ -1,8 +1,22 @@
 import { writeFileSync } from "node:fs";
+import qs from "qs";
 
-const url = "http://localhost:1337/api/reviews" + "?populate=*";
+const url =
+  "http://localhost:8484/api/reviews" +
+  "?" +
+  qs.stringify(
+    {
+      fields: ["slug", "title", "subtitle", "publishedAt"],
+      populate: { image: { fields: ["url"] } },
+      sort: ["publishedAt:desc"],
+      pagination: { pageSize: 6 },
+    },
+    { encodeValuesOnly: true }
+  );
+console.log("url:", url);
 const response = await fetch(url);
 const body = await response.json();
+console.log("body:", body);
 const formatted = JSON.stringify(body, null, 2);
 const file = "scripts/strapi-response.json";
 writeFileSync(file, formatted, "utf8");
