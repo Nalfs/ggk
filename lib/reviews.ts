d47210229bf3e4d0d1c03f5b138b8a40c90763a1
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-11-02 08:23:09 +0100

    updating libs

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -12,13 +12,17 @@ export interface Review {
   image: string;
   body: any;
 }
+interface CmsItem {
+  id: number;
+  attributes: any;
+}
 
 export async function getFeaturedReview(): Promise<Review> {
   const reviews = await getReviews();
   return reviews[0];
 }
 
-export async function getReview(slug: string) {
+export async function getReview(slug: string): Promise<any> {
   // const text = await readFile(`content/reviews/${slug}.md`, "utf-8");
   // const {
   //   content,
@@ -26,26 +30,16 @@ export async function getReview(slug: string) {
   // } = matter(text);
   // const body = marked(content);
   // return { slug, title, date, image, body };
-  const url =
-    `${CMS_URL}/api/reviews?` +
-    qs.stringify(
-      {
-        filters: { slug: { $eq: slug } },
-        fields: ["slug", "title", "subtitle", "publishedAt", "body"],
-        populate: { image: { fields: ["url"] } },
-        pagination: { pageSize: 1, withCount: false },
-      },
-      { encodeValuesOnly: true }
-    );
-  const response = await fetch(url);
-  const { data } = await response.json();
-  const { attributes } = data[0];
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+    populate: { image: { fields: ["url"] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const item = data[0];
   return {
-    slug: attributes.slug,
-    title: attributes.title,
-    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
-    image: CMS_URL + attributes.image.data.attributes.url,
-    body: marked(attributes.body),
+    ...toReview(item),
+    body: marked(item.attributes.body),
   };
 }
 
@@ -60,31 +54,40 @@ export async function getReview(slug: string) {
 //   return reviews;
 // }
 export async function getReviews(): Promise<Review[]> {
-  const url =
-    `${CMS_URL}/api/reviews` +
-    "?" +
-    qs.stringify(
-      {
-        fields: ["slug", "title", "subtitle", "publishedAt"],
-        populate: { image: { fields: ["url"] } },
-        sort: ["publishedAt:desc"],
-        pagination: { pageSize: 6 },
-      },
-      { encodeValuesOnly: true }
-    );
-  console.log("url:", url);
-  const response = await fetch(url);
-  const { data } = await response.json();
-  return data.map(({ attributes }: any) => ({
-    slug: attributes.slug,
-    title: attributes.title,
-    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
-    image: CMS_URL + attributes.image.data.attributes.url,
-  }));
+  const { data } = await fetchReviews({
+    fields: ["slug", "title", "subtitle", "publishedAt"],
+    populate: { image: { fields: ["url"] } },
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 6 },
+  });
+  return data.map(toReview);
 }
+
 export async function getSlugs(): Promise<string[]> {
   const files = await readdir("./content/reviews");
   return files
     .filter((file) => file.endsWith(".md"))
     .map((file) => file.slice(0, -".md".length));
+}
+
+async function fetchReviews(parameters: any) {
+  const url =
+    `${CMS_URL}/api/reviews?` +
+    qs.stringify(parameters, { encodeValuesOnly: true });
+  console.log("[fetchReviews]:", url);
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+  return await response.json();
+}
+
+function toReview(item: CmsItem): Review {
+  const { attributes } = item;
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+  };
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,3 +77,15 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 };
+// module.exports = {
+//   content: ["./app/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["var(--font-exo2)", "sans-serif"],
+//         orbitron: ["var(--font-orbitron)", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/typography")],
+// };
