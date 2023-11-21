6138165598beb151f753b3572b91870f90c876aa
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2023-11-21 16:18:07 +0100

    off cal

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,10 +79,10 @@ export default function RootLayout({
         <main className="flex flex-row justify-center  mx-auto py-3 shadow-lg">
           {/* <div className="bg-white p-6 shadow-lg">{children}</div>
           <div className="bg-white shadow-lg"> */}
-          <div className=" shadow-lg">{children}</div>
-          <div className=" shadow-lg">
+          <div className="shadow-lg">{children}</div>
+          {/* <div className="shadow-lg">
             <CalendarComponent />
-          </div>
+          </div> */}
         </main>
         {/* <footer className="border-t py-3 text-center text-slate-500 text-xs mb-80">
           Game Data & Images courtesy of{" "}
