79f36470c5d16fe7c0f9eae1ac94b9aeaa224d48
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-08-15 20:54:33 +0200

    test height

--- app/tww/page.tsx
+++ app/tww/page.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import Heading from "@/components/Heading";
-import { Skeleton } from "@/components/ui/skeleton"; // Example ShadCN component for loading placeholder
+// import { Skeleton } from "@/components/ui/skeleton";
 
 // Lazy load the iframe component
 const LazyIframe = React.lazy(() => import("../../components/LazyIframe"));
@@ -24,14 +24,13 @@ const page = () => {
               <div
                 style={{
                   width: "100%",
-                  height: "1200px",
+                  height: "100%",
+                  // height: "1200px",
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
                 }}
-              >
-                <Skeleton className="w-full h-full" /> {/* Loading Skeleton */}
-              </div>
+              ></div>
             }
           >
             <LazyIframe />
@@ -43,3 +42,74 @@ const page = () => {
 };
 
 export default page;
+
+// "use client";
+// import React, { useState, useEffect, Suspense } from "react";
+// import Heading from "@/components/Heading";
+// import { Skeleton } from "@/components/ui/skeleton";
+
+// // Lazy load the iframe component
+// const LazyIframe = React.lazy(() => import("../../components/LazyIframe"));
+
+// const Page = () => {
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLoading(false);
+//     }, 20); // Adjust this delay as needed to simulate loading
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="container mx-auto py-10">
+//         <div>
+//           <Heading>The War Within</Heading>
+//         </div>
+//         <div
+//           style={{
+//             maxWidth: "100%",
+//             height: "100vh",
+//             overflow: "hidden",
+//           }}
+//         >
+//           {loading ? (
+//             <div
+//               style={{
+//                 width: "100%",
+//                 height: "1200px",
+//                 display: "flex",
+//                 justifyContent: "center",
+//                 alignItems: "center",
+//               }}
+//             >
+//               <Skeleton className="w-full h-full" /> {/* Show Skeleton */}
+//             </div>
+//           ) : (
+//             <Suspense
+//               fallback={
+//                 <div
+//                   style={{
+//                     width: "100%",
+//                     height: "1200px",
+//                     display: "flex",
+//                     justifyContent: "center",
+//                     alignItems: "center",
+//                   }}
+//                 >
+//                   <Skeleton className="w-full h-full" />
+//                 </div>
+//               }
+//             >
+//               <LazyIframe />
+//             </Suspense>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Page;
