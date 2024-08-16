409374b66e33f28a5eec84f142fa8076a85f8fdf
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-08-16 13:19:52 +0200

    test

--- lib/responses.ts
+++ lib/responses.ts
@@ -2,57 +2,51 @@ import { useState, useCallback, useEffect } from "react";
 import Papa from "papaparse";
 
 /**
- * Hook for generating csv responses.
+ * Hook for fetching and parsing CSV data.
  */
 export function useGetResponses() {
   const csvUrl = process.env.NEXT_PUBLIC_CSV_URL;
-  console.log("#####URL :", csvUrl);
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<any | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const fetchResponses = useCallback(async () => {
-    setLoading(true);
+    if (!csvUrl) {
+      setError("CSV URL is not defined");
+      setLoading(false);
+      return;
+    }
+
     try {
-      if (!csvUrl) {
-        throw new Error("CSV URL is not defined");
-      }
-
-      console.log("URL value:", csvUrl);
-
       const response = await fetch(csvUrl);
-      console.log("Response:", response);
+
       if (!response.ok) {
         throw new Error(`Failed to fetch data: ${response.statusText}`);
       }
 
-      const reader = response.body?.getReader();
-      console.log("Reader:", reader);
-      if (!reader) {
-        throw new Error("No readable stream found in response");
+      const csvText = await response.text();
+
+      const parsedData = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true,
+      });
+
+      if (parsedData.errors.length > 0) {
+        throw new Error(
+          `Parsing error: ${parsedData.errors[0].message} at row ${parsedData.errors[0].row}`
+        );
       }
 
-      const { value } = await reader.read();
-      const decoder = new TextDecoder("utf-8");
-      const csv = decoder.decode(value);
-
-      console.log("CSV Data:", csv);
-
-      const parsedData = Papa.parse(csv, { header: true });
-      console.log("Parsed Data:", parsedData.data);
       setData(parsedData.data);
-    } catch (err: any) {
-      console.error("Error fetching CSV data:", err);
-      setError(err as any); // Type assertion here
+    } catch (error) {
+      setError((error as Error).message);
     } finally {
       setLoading(false);
     }
   }, [csvUrl]);
 
   useEffect(() => {
-    console.log("#1 running function");
     fetchResponses();
-    console.log("#2 running function");
   }, [fetchResponses]);
 
   return { data, loading, error };
