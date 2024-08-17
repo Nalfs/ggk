5dee6c97f4640ee647000d3fabc8897ac4e2b838
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-08-17 08:53:07 +0200

    front page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import ImageCarousel from "@/components/CarouselComponent";
 import Countdown from "@/components/Countdown";
-
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 export default async function Home() {
   return (
     <>
@@ -10,6 +16,61 @@ export default async function Home() {
         </div>
         <h1>Åh nej, du måste kommit fel!</h1>
         <ImageCarousel />
+
+        <div className="container space-y-4 my-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <Badge className="mr-2 text-white-400">8/22</Badge>
+                The War Within Early Access begins
+              </CardTitle>
+              <CardDescription>Normal Dungeons open</CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <Badge className="mr-2 text-white-400">8/26</Badge>
+                The War Within Goes Live Globally at 3:00 pm PDT
+              </CardTitle>
+              <CardDescription>
+                Heroic Dungeons and Dragonflight Season 4 PvE rewards are
+                retired.
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <Badge className="mr-2 text-white-400">9/10</Badge>
+                The War Within Season 1 Begins
+              </CardTitle>
+              <CardDescription>
+                Heroic Raids, Raid Finder Wing 1, Mythic 0 Dungeons, Heroic
+                Seasonal dungeons, a new World Boss, and PvP Season 1
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <Badge className="mr-2 text-white-400">9/17</Badge>
+                Mythic raids and Mythic+ dungeons open
+              </CardTitle>
+              <CardDescription>
+                Raid Finder Wing 2 opens, raid Story Difficulty opens
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <Badge className="mr-2 text-white-400">9/24</Badge>
+                Raid Finder Wing 3 Opens
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
       </div>
     </>
   );
