7acd7f39a7c6d840e09c3343ff6876382c510c9f
Nadim Al-Sharif <nadim.al.sharif@gmail.com>
2024-08-26 16:43:16 +0200

    WiP: component and routing done. working on request for logs.

--- app/logs/[slug]/[bossId]/page.tsx
+++ app/logs/[slug]/[bossId]/page.tsx
@@ -1,28 +1,65 @@
-// import { getBossDetails } from "@/lib/members"; // Adjust the path and function as necessary
-// import { useRouter } from "next/router";
+import { getWarcraftLogsData } from "@/lib/bosskilldata";
+import React from "react";
 
 export default async function BossDetailsPage({
   params: { slug, bossId },
 }: {
   params: { slug: string; bossId: string };
 }) {
-  //   const bossDetails = await getBossDetails(slug, bossId); // Fetch boss details using slug and bossId
+  const data = await getWarcraftLogsData(slug, bossId);
 
-  //   if (!bossDetails) {
-  //     return <p>Boss details not found</p>;
-  //   }
+  if (!data) {
+    return <p>Error fetching data</p>;
+  }
+
+  const { raidComposition, dpsData, hpsData } = data;
 
   return (
     <div className="px-2">
-      {/* <h1 className="text-2xl font-bold">{bossDetails.name}</h1> */}
+      <h1 className="text-2xl font-bold">Boss Details</h1>
       <p>
         <strong>Slug:</strong> {slug}
       </p>
       <p>
         <strong>Boss ID:</strong> {bossId}
       </p>
-      <p>{/* <strong>Description:</strong> {bossDetails.description} */}</p>
-      {/* Render additional boss details here */}
+
+      {/* Raid Composition Section */}
+      <div>
+        <h2>Raid Composition</h2>
+        {raidComposition &&
+          raidComposition.map((actor: any) => (
+            <p key={actor.id}>
+              {actor.name} - {actor.type}/{actor.subType}
+            </p>
+          ))}
+      </div>
+
+      {/* Damage Done Section */}
+      <div>
+        <h2>Damage Done</h2>
+        {dpsData.map((entry: any, index: number) => (
+          <div key={index}>
+            <p>
+              {entry.name}: {entry.dps} DPS, {entry.rankPercent}% Parse, iLvl{" "}
+              {entry.itemLevel}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      {/* Healing Done Section */}
+      <div>
+        <h2>Healing Done</h2>
+        {hpsData.map((entry: any, index: number) => (
+          <div key={index}>
+            <p>
+              {entry.name}: {entry.hps} HPS, {entry.rankPercent}% Parse, iLvl{" "}
+              {entry.itemLevel}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
